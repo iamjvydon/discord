f5f1f741a86b84317273f14c07b5a199f219361f
iamjvydon <iamjvydon@gmail.com>
2025-04-28 18:27:33 +0100

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,8 +1,11 @@
-change lavalink   {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
-      secure: false
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+        identifier: 'GlaceYT',
+        password: 'glaceyt',
+        host: '193.226.78.187',
+        port: 9372,
+        secure: false
     }
 };
